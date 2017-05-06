f256d22fa34b7d4573b9421098a74ab5c9f375a9
John <John@192.168.1.102>
2017-05-06 13:55:21 -0700

    initial commit

---
+++ ThisIsATestGitProjectYay/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args){
+		Thing1 thing1 = new Thing1();
+		Thing2 thing2 = new Thing2();
+		thing1.doTheThing();
+		thing2.doTheThing();
+		
+	}
+	
+}
